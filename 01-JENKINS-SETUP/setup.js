9ddbdbc1c8ede3d9928f5d86edf992c797594767
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2025-05-27 07:54:35 +0100

    setting up jenkins

---
+++ 01-JENKINS-SETUP/setup.js
@@ -0,0 +1,10 @@
+//create jenkins folder 
+$ mkdir $HOME / jenkins_home
+
+// run a local jenkins container
+docker run -d -p 8080:8080 \
+-v $PWD/jenkins_home:/var/jenkins_home \
+--name jenkins - ctd jenkins / jenkins
+
+// check the log to see whether jenkins is running
+docker logs jenkins-ctd 
