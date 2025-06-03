4546cc6ac3f56f69e8385417065b372322e35578
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2025-06-03 14:55:14 +0100

    added old version of jenkins

--- 01-JENKINS-SETUP/setup.js
+++ 01-JENKINS-SETUP/setup.js
@@ -1,12 +1,12 @@
 // ths will download jenkins image into your docker
-docker pull jenkins / jenkins
+docker pull jenkins/jenkins
 
 //create jenkins folder 
-$ mkdir $HOME / jenkins_home
+mkdir $HOME/jenkins_home
 
 // run a local jenkins container
 docker run -d -p 8080:8080 \
--v $PWD/jenkins_home:/var/jenkins_home \
+-v $HOME/jenkins_home:/var/jenkins_home \
 --name jenkins - ctd jenkins / jenkins
 
 // check the log to see whether jenkins is running
