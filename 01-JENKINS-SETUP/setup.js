068d02d9852ec45a774b9160fec03d462d3e2698
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2025-10-14 11:48:28 +0100

    pipeline script

--- 01-JENKINS-SETUP/setup.js
+++ 01-JENKINS-SETUP/setup.js
@@ -7,14 +7,14 @@ mkdir $HOME/jenkins_home
 // run a local jenkins container
 docker run -d -p 8080:8080 \
 -v $HOME/jenkins_home:/var/jenkins_home \
---name jenkins - ctd jenkins / jenkins
+--name jenkins - ctd jenkins/jenkins
 
 // check the log to see whether jenkins is running
 docker logs jenkins-ctd 
 
 
 // log into jenkins container 
-docker exec - it old - jenkins bash
+docker exec -it old - jenkins bash
 docker exec -it jenkins bash
 
 
