39a2f056ef1435fb5caf97f10d137494b4fb5c17
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2025-10-15 12:11:15 +0100

    checkout

--- 02-PIPELINE-SETUP/pipeline.js
+++ 02-PIPELINE-SETUP/pipeline.js
@@ -2,3 +2,10 @@
 CLICK "NEW ITEM" >> name your pipeline >> CLICK "PIPELINE"
 GO TO PIPELINE SECTION 
 add hello-world.jenkins to the script section 
+
+// STEP 2 CREATE A GITHUB REPOSITORY
+Go to github.com  if you don'tClick on New, next to Reposi
+Give it a name – calculator
+click create
+
+
