339de3bdca7abb5d5d9209672c2add0c6ebde493
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2026-06-15 10:36:59 +0100

    ci setup

---
+++ 02-PIPELINE-SETUP/ci-setup.js
@@ -0,0 +1,27 @@
+// STEP 1 INSTALL THE FOLLOWING PLUGINS
+Dashboard
+v
+Mange Jenkins
+v
+Plugins
+v
+Available Plugins
+
+
+git
+Git Client 
+Pipeline 
+Pipeline: Stage View 
+Maven Integration 
+Credentials Binding 
+SSH Agent 
+JUnit 
+Workspace Cleanup 
+Blue Ocean 
+SonarQube Scanner
+
+
+// STEP 2: Configure Git in Jenkins
+Navigate to: 
+Manage Jenkins
+v
